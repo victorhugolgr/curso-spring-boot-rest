72f463a05c73c310893fa8b5659c3d657c80cf23
Victor Hugo <victorhugolgr@gmail.com>
2017-07-31 23:12:04 -0300

    2

--- src/main/java/com/example/algamoneyapi/resource/CateriaResource.java
+++ src/main/java/com/example/algamoneyapi/resource/CateriaResource.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.algamoneyapi.model.Categoria;
 import com.example.algamoneyapi.repository.CategoriaRepository;
 
-
 @RestController
 @RequestMapping("/categorias")
 public class CateriaResource {
